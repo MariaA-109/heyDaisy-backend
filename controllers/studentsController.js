070b2c45f1b1219e14e5fcbaf1bc41ad11c42f0c
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-06-16 14:43:47 +0200

    changed file names

---
+++ controllers/studentsController.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function studentsController() {
+  return <div>studentsController</div>;
+}
+
+export default studentsController;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllUsers } = require("../controllers/users");
+const { getAllUsers } = require("../controllers/usersController");
 
 router.route("/auth/users").get(getAllUsers);
 
