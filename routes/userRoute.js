f93e5b06304b40f6beffdb46b19f0088370ec505
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-06-20 15:40:22 +0200

    fixed middleware problem

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,9 +6,9 @@ const {
   signUp,
   logIn,
 } = require("../controllers/usersController");
-const auth = require("../middlewares/auth");
+//const auth = require("../middlewares/auth");
 
-router.route("/auth/users").get(auth, getAllUsers);
+router.route("/users").get(getAllUsers);
 router.route("/auth/signup").post(signUp);
 router.route("/auth/login").post(logIn);
 
